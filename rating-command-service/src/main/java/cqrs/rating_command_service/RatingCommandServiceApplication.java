4bb298d4663cbf30a43e815c44d7809d32707e01
nothiaki <celsohenrique367@gmail.com>
2025-03-12 21:40:08 -0300

    feat: rating-command-service project

---
+++ rating-command-service/src/main/java/cqrs/rating_command_service/RatingCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package cqrs.rating_command_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingCommandServiceApplication.class, args);
+	}
+
+}
---
+++ rating-command-service/src/test/java/cqrs/rating_command_service/RatingCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cqrs.rating_command_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
