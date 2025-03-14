3e8a946054b27b26a8737990e3f59d47ff602ce6
nothiaki <celsohenrique367@gmail.com>
2025-03-13 23:58:08 -0300

    feat: rating controller

---
+++ rating-command-service/src/main/java/cqrs/rating_command_service/application/controller/RatingController.java
@@ -0,0 +1,29 @@
+package cqrs.rating_command_service.application.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cqrs.rating_command_service.core.domain.Rating;
+import cqrs.rating_command_service.core.usecase.RatingUseCase;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController {
+
+  private final RatingUseCase ratingUseCase;
+
+  public RatingController(RatingUseCase ratingUseCase) {
+    this.ratingUseCase = ratingUseCase;
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> create(@RequestBody Rating rating) {
+    ratingUseCase.create(rating);
+    return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+  }
+
+}
