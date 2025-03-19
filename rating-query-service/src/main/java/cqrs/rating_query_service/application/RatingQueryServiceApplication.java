06c45803fb7a5fc7985f4c910f94bbf71cf25a69
nothiaki <celsohenrique367@gmail.com>
2025-03-19 20:51:51 -0300

    feat: rating query service

---
+++ rating-query-service/src/main/java/cqrs/rating_query_service/application/RatingQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package cqrs.rating_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingQueryServiceApplication.class, args);
+	}
+
+}
