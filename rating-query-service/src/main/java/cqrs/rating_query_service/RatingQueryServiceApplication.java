66b478d8bd6b6cf8bb5aca41b53893bc849d53e1
nothiaki <celsohenrique367@gmail.com>
2025-03-23 15:51:11 -0300

    fix: remove elasticsearch implementation

--- rating-query-service/src/main/java/cqrs/rating_query_service/RatingQueryServiceApplication.java
+++ rating-query-service/src/main/java/cqrs/rating_query_service/RatingQueryServiceApplication.java
@@ -1,4 +1,4 @@
-package cqrs.rating_query_service.application;
+package cqrs.rating_query_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- rating-query-service/src/main/java/cqrs/rating_query_service/application/usecase/messaging/ConsumerUseCaseImpl.java
+++ rating-query-service/src/main/java/cqrs/rating_query_service/application/usecase/messaging/ConsumerUseCaseImpl.java
@@ -1,47 +1,24 @@
 package cqrs.rating_query_service.application.usecase.messaging;
 
-import java.util.UUID;
-
 import org.springframework.stereotype.Service;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.core.IndexRequest;
 import cqrs.rating_query_service.core.domain.Rating;
 import cqrs.rating_query_service.core.shared.StringHandler;
 import cqrs.rating_query_service.core.usecase.messaging.ConsumerUseCase;
-import cqrs.rating_query_service.infrastructure.config.elasticsearch.ElasticSearchConfig;
 
 @Service
 public class ConsumerUseCaseImpl implements ConsumerUseCase {
 
   private final StringHandler<Rating> stringHandler;
-  private final ElasticSearchConfig elasticSearchConfig;
 
   public ConsumerUseCaseImpl(
-    StringHandler<Rating> stringHandler,
-    ElasticSearchConfig elasticSearchConfig
+    StringHandler<Rating> stringHandler
   ) {
     this.stringHandler = stringHandler;
-    this.elasticSearchConfig = elasticSearchConfig;
   }
 
   @Override
   public void ratingCreate(String payload) {
     Rating rating = stringHandler.deserialize(payload, Rating.class);
-
-    ElasticsearchClient elasticsearchClient = elasticSearchConfig.elasticsearchClient();
-
-    IndexRequest<Rating> indexRequest = new IndexRequest.Builder<Rating>()
-      .index("rating")
-      .id(UUID.randomUUID().toString())
-      .document(rating)
-      .build();
-
-    try {
-      elasticsearchClient.index(indexRequest);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
   }
 }
