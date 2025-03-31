5d003d89808c9258b7b553c30730f6d44d1a1874
nothiaki <celsohenrique367@gmail.com>
2025-03-31 16:58:34 -0300

    feat: rating controller

---
+++ rating-query-service/src/main/java/cqrs/rating_query_service/application/controller/RatingController.java
@@ -0,0 +1,26 @@
+package cqrs.rating_query_service.application.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cqrs.rating_query_service.core.domain.Rating;
+import cqrs.rating_query_service.core.usecase.RatingUseCase;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController {
+
+  private final RatingUseCase ratingUseCase;
+
+  public RatingController(RatingUseCase ratingUseCase) {
+    this.ratingUseCase = ratingUseCase;
+  }
+
+  public ResponseEntity<List<Rating>> findRating() {
+    return ResponseEntity.ok(ratingUseCase.findRating());
+  }
+
+}
