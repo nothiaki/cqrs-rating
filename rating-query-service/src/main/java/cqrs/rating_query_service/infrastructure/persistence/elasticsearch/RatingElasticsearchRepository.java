0d865da52eabdb5a51765bb4f3b1846c14eea149
nothiaki <celsohenrique367@gmail.com>
2025-04-07 20:31:17 -0300

    fix: elasticsearch entity model

--- rating-query-service/src/main/java/cqrs/rating_query_service/infrastructure/persistence/elasticsearch/RatingElasticsearchRepository.java
+++ rating-query-service/src/main/java/cqrs/rating_query_service/infrastructure/persistence/elasticsearch/RatingElasticsearchRepository.java
@@ -1,10 +1,8 @@
 package cqrs.rating_query_service.infrastructure.persistence.elasticsearch;
 
-import java.util.UUID;
-
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RatingElasticsearchRepository extends ElasticsearchRepository<RatingEntity, UUID> {}
+public interface RatingElasticsearchRepository extends ElasticsearchRepository<RatingEntity, String> {}
 
--- rating-query-service/src/main/java/cqrs/rating_query_service/infrastructure/persistence/elasticsearch/RatingEntity.java
+++ rating-query-service/src/main/java/cqrs/rating_query_service/infrastructure/persistence/elasticsearch/RatingEntity.java
@@ -1,60 +1,139 @@
 package cqrs.rating_query_service.infrastructure.persistence.elasticsearch;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import cqrs.rating_query_service.core.domain.Rating;
-
 @Document(indexName = "postgres.rating-command-service.public.rating")
 public class RatingEntity {
-
+    
   @Id
-  private UUID id;
-  private UUID ownerId;
-  private UUID productId;
-  private Integer rate;
-  private String feedback;
-  private LocalDateTime created;
+  private String id;
+
+  @Field(name = "payload")
+  private Payload payload;
 
   public RatingEntity() {}
-
-  public RatingEntity(
-    UUID id,
-    UUID ownerId,
-    UUID productId,
-    Integer rate,
-    String feedback,
-    LocalDateTime created
-  ) {
-    this.id = id;
-    this.ownerId = ownerId;
-    this.productId = productId;
-    this.rate = rate;
-    this.feedback = feedback;
-    this.created = created;
+    
+  public String getId() {
+    return this.id;
   }
 
-  public RatingEntity fromDomainToEntity(Rating rating) {
-    return new RatingEntity(
-      rating.getId(),
-      rating.getOwnerId(),
-      rating.getProductId(),
-      rating.getRate(),
-      rating.getFeedback(),
-      rating.getCreated()
-    );
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Payload getPayload() {
+    return this.payload;
+  }
+
+  public void setPayload(Payload payload) {
+    this.payload = payload;
+  }
+
+  public static class Payload {
+
+    @Field(name = "after")
+    private After after;
+
+    public After getAfter() {
+      return this.after;
+    }
+
+    public void setAfter(After after) {
+      this.after = after;
+    }
+
+  }
+
+  public static class After {
+
+    @Field(name = "id")
+    private String id;
+
+    @Field(name = "owner_id")
+    private String ownerId;
+
+    @Field(name = "product_id")
+    private String productId;
+
+    @Field(name = "rate")
+    private Integer rate;
+
+    @Field(name = "feedback")
+    private String feedback;
+
+    @Field(name = "created")
+    private Long created;
+
+    public String getId() {
+      return id;
+    }
+
+    public void setId(String id) {
+      this.id = id;
+    }
+
+    public String getOwnerId() {
+      return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+      this.ownerId = ownerId;
+    }
+
+    public String getProductId() {
+      return productId;
+    }
+
+    public void setProductId(String productId) {
+      this.productId = productId;
+    }
+
+    public Integer getRate() {
+      return rate;
+    }
+
+    public void setRate(Integer rate) {
+      this.rate = rate;
+    }
+
+    public String getFeedback() {
+      return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+      this.feedback = feedback;
+    }
+
+    public Long getCreated() {
+      return created;
+    }
+
+    public void setCreated(Long created) {
+      this.created = created;
+    }
+      
   }
 
   public Rating fromEntityToDomain() {
+    After data = this.getPayload().getAfter();
+    LocalDateTime created = Instant.ofEpochMilli(data.getCreated())
+      .atZone(ZoneId.systemDefault())
+      .toLocalDateTime();
+
     return new Rating(
-      id,
-      ownerId,
-      productId,
-      rate,
-      feedback,
+      UUID.fromString(data.getId()),
+      UUID.fromString(data.getOwnerId()),
+      UUID.fromString(data.getProductId()),
+      data.getRate(),
+      data.getFeedback(),
       created
     );
   }
