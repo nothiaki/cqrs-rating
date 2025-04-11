a967f5521b9cd11fc69497dbffd32e79387209a8
nothiaki <celsohenrique367@gmail.com>
2025-04-11 19:14:19 -0300

    chore: remove message producer comments

--- rating-command-service/src/main/java/cqrs/rating_command_service/application/usecase/RatingUseCaseImpl.java
+++ rating-command-service/src/main/java/cqrs/rating_command_service/application/usecase/RatingUseCaseImpl.java
@@ -6,28 +6,19 @@ import org.springframework.stereotype.Service;
 
 import cqrs.rating_command_service.core.domain.Rating;
 import cqrs.rating_command_service.core.repository.RatingRepository;
-//import cqrs.rating_command_service.core.shared.StringHandler;
 import cqrs.rating_command_service.core.usecase.RatingUseCase;
 import cqrs.rating_command_service.core.usecase.SourceUseCase;
-//import cqrs.rating_command_service.core.usecase.messaging.MessagingTopics;
-//import cqrs.rating_command_service.core.usecase.messaging.ProducerUseCase;
 
 @Service
 public class RatingUseCaseImpl implements RatingUseCase{
 
-  //private final ProducerUseCase producerUseCase;
-  //private final StringHandler<Rating> stringHandler;
   private final RatingRepository ratingRepository;
   private final SourceUseCase sourceUseCase;
 
   public RatingUseCaseImpl(
-    //ProducerUseCase producerUseCase,
-    //StringHandler<Rating> stringHandler,
     RatingRepository ratingRepository,
     SourceUseCase sourceUseCase
   ) {
-    //this.producerUseCase = producerUseCase;
-    //this.stringHandler = stringHandler;
     this.ratingRepository = ratingRepository;
     this.sourceUseCase = sourceUseCase;
   }
@@ -39,10 +30,6 @@ public class RatingUseCaseImpl implements RatingUseCase{
     Rating ratingSaved = ratingRepository.save(rating);
 
     sourceUseCase.createRating(ratingSaved);
-
-    //String stringfiedRating = stringHandler.serialize(ratingSaved);
-
-    //producerUseCase.send(MessagingTopics.RATING_CREATE, stringfiedRating);
   }
 
 }
